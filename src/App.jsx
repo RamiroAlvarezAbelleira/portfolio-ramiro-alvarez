042d8d073cbde5f8a6057697b1c6e2acc239a8c1
RamiroAlvarezAbelleira <alvarezramiro1997@gmail.com>
2023-10-01 00:04:58 -0300

    basic about and skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,54 @@
+import { Col, Container, Row } from 'react-bootstrap'
 import './App.css'
 import { Header } from './components/Header/'
 import { Footer } from './components/footer'
+import { BsGithub } from 'react-icons/bs'
+import { FaReact, FaCss3Alt, FaHtml5, FaBootstrap, FaGit } from 'react-icons/fa'
+import { SiRubyonrails, SiJavascript, SiAxios, SiRedux, SiPostgresql } from 'react-icons/si'
 
 function App() {
-
+  const icons = [ <FaGit />, <BsGithub />, <FaReact />, <SiRubyonrails />, <SiJavascript />, <FaCss3Alt />, <FaHtml5 />, <SiAxios />, <SiRedux />, <SiPostgresql />, <FaBootstrap /> ]
   return (
-    <div>
+    <div className='page-container'>
       <Header />
-      <h1>Portafolio</h1>
-      <section id='about-me'>
-        <h1>About me.</h1>
-        <p>some descriptive text about my self would be placed right here!</p>
-      </section>
-      <section id='projects'>
-        <h1>My Projects.</h1>
-        <p>List of projects</p>
-      </section>
-      <section id='skills'>
-        <h3>My Skills</h3>
-        <p>all my skills would be here</p>
-      </section>
+      <Container className='main-container'>
+        <section id="main">
+          <h1>Ramiro Alvarez Abelleira.</h1>
+          <h3>Full Stack Web Developer.</h3>
+        </section>
+        
+        <section id='about-me'>
+          <div>
+            <h3>About me.</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto aut rerum doloribus. Commodi, provident. 
+              Sunt magnam natus asperiores temporibus cum quaerat sapiente praesentium in. 
+              Doloribus ipsa omnis consectetur deleniti! Reprehenderit sint consectetur nulla. 
+              Architecto quasi totam quisquam tempore laudantium distinctio accusamus necessitatibus deserunt animi eveniet aliquam, 
+              quia ex beatae expedita?</p>
+          </div>
+        </section>
+        <section id='projects'>
+          <h3>My Projects.</h3>
+          <p>List of projects</p>
+        </section>
+        <section id='skills'>
+          <h3>My Skills</h3>
+          <Row >
+            {
+              ['Git', 'GitHub', 'React', 'Ruby on Rails', 'JavaScript', 'Css', 'Html', 'Axios', 'Redux', 'PostgreSQL', 'Bootstrap'].map((tool, i) => {
+                return <Col className='tool'>
+                          {icons[i]}
+                          {tool}
+                        </Col>
+              }) 
+            }
+          </Row>
+        </section>
+        <section id='contact'>
+          <h3>contact me</h3>
+          <p>Form</p>
+        </section>
+      </Container>
       <Footer />
     </div>
   )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,25 +5,26 @@ import { Container, Nav, Navbar, Offcanvas } from 'react-bootstrap'
 const Header = () => {
   return (
     <header className='header'>
-        <Navbar expand="lg">
+        <Navbar collapseOnSelect expand="lg" data-bs-theme="dark" fixed='top'>
           <Container fluid>
-            <Navbar.Brand href="#">Ramiro Alvarez Abelleira</Navbar.Brand>
+            <Navbar.Brand href="#main" className='primary-text-color'></Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand`} />
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand`}
               aria-labelledby={`offcanvasNavbarLabel-expand`}
               placement="end"
             >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand`}>
+              <Offcanvas.Header closeButton className='bg-dark'>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand`} className='primary-text-color'>
                   Portfolio
                 </Offcanvas.Title>
               </Offcanvas.Header>
-              <Offcanvas.Body>
+              <Offcanvas.Body className='bg-dark'>
                 <Nav>
-                  <Nav.Link href="#about-me">About me</Nav.Link>
-                  <Nav.Link href="#projects">Projects</Nav.Link>
-                  <Nav.Link href="#skills">Skills</Nav.Link>
+                  <Nav.Link href="#about-me" className='secondary-text-color'>About me</Nav.Link>
+                  <Nav.Link href="#projects" className='secondary-text-color'>Projects</Nav.Link>
+                  <Nav.Link href="#skills" className='secondary-text-color'>Skills</Nav.Link>
+                  <Nav.Link href="#contact" className='secondary-text-color'>Contact</Nav.Link>
                 </Nav>
               </Offcanvas.Body>
             </Navbar.Offcanvas>
